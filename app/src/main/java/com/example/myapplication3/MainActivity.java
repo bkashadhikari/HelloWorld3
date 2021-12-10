7b122f69a5042193fa5bf82676af514d8106073e
bkashadhikari <abikash21@tbc.edu.np>
2021-12-11 00:19:52 +0545

    task2done

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,14 +11,18 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button Clicked");
+        Log.d(LOG_TAG,"Button Clicked!");
+        Intent intent = new Intent(this, com.example.myapplication3.SecondActivity.class);
+        startActivity(intent);
     }
 }
-
