13137a2215e35eabf15e28e8de98982330c4f1ce
bkashadhikari <abikash21@tbc.edu.np>
2021-12-14 13:32:27 +0545

    Task3

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
+    private EditText mMessageEditText;
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.myapplication3.extra.MESSAGE";
+
+
 
 
 
@@ -17,12 +23,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mMessageEditText = findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG,"Button Clicked!");
-        Intent intent = new Intent(this, com.example.myapplication3.SecondActivity.class);
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
