5cecbdb98f3068d4a98bcf74358b41a7a12a9cd9
bkashadhikari <abikash21@tbc.edu.np>
2021-12-23 16:55:14 +0545

    task4

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -7,8 +7,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextview;
+    private TextView mReplyTextview;
+
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
     private EditText mMessageEditText;
@@ -24,6 +29,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextview = findViewById(R.id.text_header_reply);
+        mReplyTextview = findViewById(R.id.text_message_reply);
+    }
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply =
+                        data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextview.setVisibility(View.VISIBLE);
+                mReplyTextview.setText(reply);
+                mReplyTextview.setVisibility(View.VISIBLE);
+            }
+        }
     }
 
     public void launchSecondActivity(View view) {
@@ -31,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SecondActivity.class);
         String message = mMessageEditText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent,TEXT_REQUEST);
         startActivity(intent);
     }
 }
